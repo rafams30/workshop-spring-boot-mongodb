3f7dd4bf9dfc6bb1ef0b6156c71e52634fc12c30
Rafael Martines <rafams_22@hotmail.com>
2024-04-16 08:58:46 -0300

    Inserção de usuário com POST

--- src/main/java/com/rafaelmartines/workshopmongo/services/UserService.java
+++ src/main/java/com/rafaelmartines/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.rafaelmartines.workshopmongo.services;
 
 import com.rafaelmartines.workshopmongo.domain.User;
+import com.rafaelmartines.workshopmongo.dto.UserDTO;
 import com.rafaelmartines.workshopmongo.repository.UserRepository;
 import com.rafaelmartines.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
