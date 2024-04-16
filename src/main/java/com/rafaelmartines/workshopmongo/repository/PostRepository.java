3cfcb82daa9608d5c290460996b9105374875326
Rafael Martines <rafams_22@hotmail.com>
2024-04-16 09:53:00 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/rafaelmartines/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.rafaelmartines.workshopmongo.repository;
+
+import com.rafaelmartines.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
