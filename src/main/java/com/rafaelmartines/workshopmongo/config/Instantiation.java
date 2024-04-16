74feb7e5359a6025147624acbd001371cef4adf6
Rafael Martines <rafams_22@hotmail.com>
2024-04-16 14:27:49 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/rafaelmartines/workshopmongo/config/Instantiation.java
+++ src/main/java/com/rafaelmartines/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.rafaelmartines.workshopmongo.config;
 import com.rafaelmartines.workshopmongo.domain.Post;
 import com.rafaelmartines.workshopmongo.domain.User;
 import com.rafaelmartines.workshopmongo.dto.AuthorDTO;
+import com.rafaelmartines.workshopmongo.dto.CommentDTO;
 import com.rafaelmartines.workshopmongo.repository.PostRepository;
 import com.rafaelmartines.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2024"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2024"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2024"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/rafaelmartines/workshopmongo/resource/PostResource.java
+++ src/main/java/com/rafaelmartines/workshopmongo/resource/PostResource.java
@@ -1,18 +1,11 @@
 package com.rafaelmartines.workshopmongo.resource;
 
 import com.rafaelmartines.workshopmongo.domain.Post;
-import com.rafaelmartines.workshopmongo.domain.User;
-import com.rafaelmartines.workshopmongo.dto.UserDTO;
+
 import com.rafaelmartines.workshopmongo.services.PostService;
-import com.rafaelmartines.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
