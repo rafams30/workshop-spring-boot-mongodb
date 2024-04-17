251a6b874ecbfb18cd5d1bc64420b1f66455ca5a
Rafael Martines <rafams_22@hotmail.com>
2024-04-17 08:48:19 -0300

    Consulta simples com query methods

--- src/main/java/com/rafaelmartines/workshopmongo/resource/PostResource.java
+++ src/main/java/com/rafaelmartines/workshopmongo/resource/PostResource.java
@@ -2,10 +2,13 @@ package com.rafaelmartines.workshopmongo.resource;
 
 import com.rafaelmartines.workshopmongo.domain.Post;
 
+import com.rafaelmartines.workshopmongo.resource.util.URL;
 import com.rafaelmartines.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -18,6 +21,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/com/rafaelmartines/workshopmongo/services/PostService.java
+++ src/main/java/com/rafaelmartines/workshopmongo/services/PostService.java
@@ -1,10 +1,7 @@
 package com.rafaelmartines.workshopmongo.services;
 
 import com.rafaelmartines.workshopmongo.domain.Post;
-import com.rafaelmartines.workshopmongo.domain.User;
-import com.rafaelmartines.workshopmongo.dto.UserDTO;
 import com.rafaelmartines.workshopmongo.repository.PostRepository;
-import com.rafaelmartines.workshopmongo.repository.UserRepository;
 import com.rafaelmartines.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +18,10 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 
 }
